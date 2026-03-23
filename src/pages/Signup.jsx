6c5d80b8402e538d480a1f70f50eedc4797858b3
oppod_j <oppod_j@ca01.ub.lan>
2026-03-23 13:01:02 +0800

    Implemented validation logic

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -7,18 +7,55 @@ function Signup() {
     const [password, setpassword] = useState("");
     const [email, setemail] = useState("");
     const navigate = useNavigate();
+    const [errors, setErrors] = useState({});
+
+    const validate = () => {
+        const newErrors = {};
+        if (!/^[A-Za-z]+$/.test(name)) {
+            newErrors.name = "Names can only contain letters.";
+            }
+        if (!/^.{2,}$/.test(name)) {
+            newErrors.name = "Name must be at least 2 characters long.";
+            }
+        if (!/^[A-Za-z0-9._-]+$/.test(username)) newErrors.username 
+            = "The text must be at least one character long and can only contain letters, numbers, dots, underscores, or hyphens.";
+        if (!/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,16}$/.test(password)) newErrors.password = "Password is required";
+        
+        if (!/^.{8,16}$/.test(password)) {
+        newErrors.password = "Password must be between 8 and 16 characters.";
+        }
+
+        if (!/[a-z]/.test(password)) {
+        newErrors.password = "Password must contain at least one lowercase letter.";
+        }
+
+        if (!/[A-Z]/.test(password)) {
+        newErrors.password = "Password must contain at least one uppercase letter.";
+        }
+
+        if (!/\d/.test(password)) {
+        newErrors.password = "Password must contain at least one number.";
+        }
+
+        if (!/[\W_]/.test(password)) {
+        newErrors.password = "Password must contain at least one special character.";
+        }
+
+        if (!/\S+@\S+\.\S+/.test(email)) newErrors.email = "Email is invalid";
+        setErrors(newErrors);
+        return Object.keys(newErrors).length === 0;
+        };
 
     const handleSubmit = (e) => {
         e.preventDefault();
 
-        console.log(name, username, password, email);
-
+        if (validate()) {
+            console.log(name, username, password, email);
+        }
         navigate("/success");
     }
-
-
     return(
-        <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit}>
             <div>
                 <label>Name:</label>
                 <input type="text" value={name} onChange={(e) => 

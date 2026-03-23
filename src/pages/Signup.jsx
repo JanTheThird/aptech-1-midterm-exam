862e91963c6db308badfb8a6fec4f2b3ee99ed53
oppod_j <oppod_j@ca01.ub.lan>
2026-03-23 12:10:20 +0800

    Implemented event handling

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,8 +1,46 @@
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+
 function Signup() {
+    const [name, setname] = useState("");
+    const [username, setusername] = useState("");
+    const [password, setpassword] = useState("");
+    const [email, setemail] = useState("");
+    const navigate = useNavigate();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        console.log(name, username, password, email);
+
+        navigate("/success");
+    }
+
+
     return(
-        <div>
-            <h1>Signup Page</h1>
-        </div>
+        <form onSubmit={handleSubmit}>
+            <div>
+                <label>Name:</label>
+                <input type="text" value={name} onChange={(e) => 
+                    setname(e.target.value)} />
+            </div>
+            <div>
+                <label>Username:</label>
+                <input type="text" value={username} onChange={(e) => 
+                    setusername(e.target.value)} />
+            </div>
+            <div>
+                <label>Password:</label>
+                <input type="password" value={password} onChange={(e) => 
+                    setpassword(e.target.value)} />
+            </div>
+            <div>
+                <label>Email:</label>
+                <input type="email" value={email} onChange={(e) => 
+                    setemail(e.target.value)} />
+            </div>
+            <button type="submit">Signup</button>
+        </form>
     )
 }
 
